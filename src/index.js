bd814f4821931a9ec6001460c7b979072a0d1705
syd333 <sydmiranda@icloud.com>
2021-02-27 15:57:11 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const form = document.getElementById('create-task-form')
+
+  form.addEventListener('submit', function(e){
+
+    const input = document.getElementById('new-task-description')
+    e.preventDefault()
+
+    const ul = document.getElementById('tasks')
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+    const text = document.createElement('text')
+
+    btn.textContent ='x'
+    text.textContent = input.value + ' '
+    btn.addEventListener('click', function (e){
+      e.target.parentNode.remove()
+    })
+
+    li.appendChild(text)
+    ul.appendChild(li)
+    li.appendChild(btn)
+
+    input.value = ""
+
+  })
+
+})
